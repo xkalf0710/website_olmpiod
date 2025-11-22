4b1a81147867d4c77ced443ac6690629528d9b13
xkalf0710 <mnymsuren37@gmail.com>
2025-11-22 20:52:06 +0800

    add

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -2,8 +2,20 @@
 function Home(){
     return (
         <>
-        <div> 
-            Home Page
+        <div className="bg-white min-h-screen w-full"> 
+            <header className="bg-gradient-to-b from-[#2c1c12] to-[#000000] text-white shadow-md border-b border-black">
+                <div className="flex items-center justify-between px-4 sm:px-10 md:px-20 lg:px-40 py-4">
+                    <a href="#" className="flex flex-row items-center gap-2">
+                        <img
+                            src="./assests/logo.jpg"
+                            alt=""
+                            className="h-10 w-auto"
+                        />
+                        <span className="text-lg font-bold text-white">Туршилтын веб сайт</span>
+                   </a>
+
+                </div>
+            </header>
         </div>
         </>
     );
